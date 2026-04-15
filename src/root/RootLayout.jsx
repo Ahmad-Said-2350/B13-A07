6e9bb27552b96639a6769de0976a1f3a1186f5c2
Ahmad-Said-2350 <mail.saydorrahman@gmail.com>
2026-04-15 16:26:53 +0600

    Routes File Added

--- src/root/RootLayout.jsx
+++ src/root/RootLayout.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import NavBar from '../components/NavBar';
+import { Outlet } from 'react-router';
+import Footer from '../components/Footer';
 
 const RootLayout = () => {
     return (
         <div>
-            
+            <NavBar></NavBar>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
+
+
+
 
 export default RootLayout;
